423f9cdfd77b1047f7f63076c90bdd582a49abea
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-16 09:29:22 -0600

    Setup schema for characters and searches tables

---
+++ db/migrate/20210616152619_create_characters.rb
@@ -0,0 +1,16 @@
+class CreateCharacters < ActiveRecord::Migration[6.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.integer :height
+      t.integer :mass
+      t.string :hair_color
+      t.string :skin_color
+      t.string :eye_color
+      t.string :birth_year
+      t.string :gender
+
+      t.timestamps
+    end
+  end
+end
