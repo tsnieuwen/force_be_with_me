acf63bf9fefe1d7dbf07b9fd66e2d22c1a3e607b
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-16 11:59:34 -0600

    Additional sad paths for character model

--- spec/models/character_spec.rb
+++ spec/models/character_spec.rb
@@ -12,5 +12,22 @@ RSpec.describe Character, type: :model do
     it { should validate_presence_of :birth_year }
     it { should validate_presence_of :gender }
     it { should validate_uniqueness_of :name }
+
+    it "happy path creates a character record with valid params" do
+      character = Character.new(name: "Luke", height: 23, mass: 12, hair_color: "brown", skin_color: "blue", eye_color: "green", birth_year: "1234", gender: "male")
+      expect(character.save).to eq(true)
+    end
+
+    it "sad path missing required param" do
+      character = Character.new(name: "Luke", height: 23, mass: 12, skin_color: "blue", eye_color: "green", birth_year: "1234", gender: "male")
+      expect(character.save).to eq(false)
+    end
+
+    it "sad path character name not unique" do
+      character1 = Character.new(name: "Luke", height: 23, mass: 12, hair_color: "brown", skin_color: "blue", eye_color: "green", birth_year: "1234", gender: "male")
+      character1.save
+      character2 = Character.new(name: "Luke", height: 34, mass: 11, hair_color: "blonde", skin_color: "red", eye_color: "brown", birth_year: "432", gender: "female")
+      expect(character2.save).to eq(false)
+    end
   end
 end
