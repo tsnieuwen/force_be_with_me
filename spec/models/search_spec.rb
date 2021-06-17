75b630f8b112a576821b117560dcd4b1a3f8d12f
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-16 18:57:11 -0600

    Update searches model and spec to reflect column changes

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -3,25 +3,33 @@ require 'rails_helper'
 RSpec.describe Search, type: :model do
 
   describe "validations" do
-    it { should validate_uniqueness_of(:name).scoped_to( :height, :mass, :hair_color, :skin_color, :eye_color, :birth_year, :gender) }
+    it { should validate_uniqueness_of(:name).scoped_to(:heavier_than,
+                                                        :lighter_than,
+                                                        :taller_than,
+                                                        :shorter_than,
+                                                        :hair_color,
+                                                        :skin_color,
+                                                        :eye_color,
+                                                        :birth_year,
+                                                        :gender) }
   end
 
   it "happy path creates a search record" do
-    search = Search.new(name: "Luke", height: 23)
+    search = Search.new(name: "Luke", taller_than: 23)
     expect(search.save).to eq(true)
   end
 
   it "happy path creates a search record with some identical params" do
-    search1 = Search.new(name: "Luke", height: 23, mass: 345)
+    search1 = Search.new(name: "Luke", taller_than: 23, lighter_than: 345)
     search1.save
-    search2 = Search.new(name: "Luke", height: 23, mass: 45)
+    search2 = Search.new(name: "Luke", taller_than: 23, lighter_than: 45)
     expect(search2.save).to eq(true)
   end
 
   it "sad path does not create a search record with all identical params" do
-    search1 = Search.new(name: "Luke", height: 23, mass: 345)
+    search1 = Search.new(name: "Luke", taller_than: 23, lighter_than: 345)
     search1.save
-    search2 = Search.new(name: "Luke", height: 23, mass: 345)
+    search2 = Search.new(name: "Luke", taller_than: 23, lighter_than: 345)
     expect(search2.save).to eq(false)
   end
 
