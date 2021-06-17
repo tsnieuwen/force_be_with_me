a50e70fed0468dec456efeba241070eca3efa9ed
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-16 18:46:23 -0600

    Progress on character search endpoint

--- app/controllers/api/v1/characters/search_controller.rb
+++ app/controllers/api/v1/characters/search_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::Characters::SearchesController < ApplicationController
 
-  def post
+  def create
+    Search.create(search_params)
+    searched_characters = Character.discover_characters(search_params)
   end
 
+  private
+
+  def search_params
+    params.permit(:name,
+                  :taller_than,
+                  :shorter_than,
+                  :heavier_than,
+                  :lighter_than,
+                  :hair_color,
+                  :skin_color,
+                  :eye_color,
+                  :birth_year,
+                  :gender)
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,15 @@ class Character < ApplicationRecord
             presence: true
 
   validates :name, uniqueness: true
+
+  def self.discover_characters(search)
+    characters = Character.all
+    characters = characters.where('name ILIKE ?', "#{search[:name]}") if search[:name].present?
+    characters = characters.where(hair_color: "#{search[:hair_color]}") if search[:hair_color].present?
+    characters = characters.where(skin_color: "#{search[:skin_color]}") if search[:skin_color].present?
+    characters = characters.where(eye_color: "#{search[:eye_color]}") if search[:eye_color].present?
+    characters = characters.where(birth_year: "#{search[:birth_year]}") if search[:birth_year].present?
+    characters = characters.where(gender: "#{search[:gender]}") if search[:gender].present?
+
+  end
 end
