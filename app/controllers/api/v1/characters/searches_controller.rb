114e962132c5edb940a939253f23a329cdef31b8
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-23 16:50:12 -0600

    Happy and sad path testing for searches controller

--- app/controllers/api/v1/characters/searches_controller.rb
+++ app/controllers/api/v1/characters/searches_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::Characters::SearchesController < ApplicationController
     search = Search.new(search_params)
     if search.save
       searched_characters = Character.discover_characters(search_params)
-      render json: CharactersSerializer.new(searched_characters)
+      rendering_character(searched_characters)
     else
       render json: "Search could not be executed", status: 400
     end
@@ -25,4 +25,13 @@ class Api::V1::Characters::SearchesController < ApplicationController
                   :birth_year,
                   :gender)
   end
+
+  def rendering_character(searched_characters)
+    if searched_characters.empty?
+      render json: "No records matched the given search", status: 200
+    else
+      render json: CharactersSerializer.new(searched_characters)
+    end
+  end
+
 end
---
+++ spec/requests/api/v1/post_search_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+describe "Post searches" do
+  before(:each) do
+    VCR.use_cassette('import_characters_request_spec') do
+      Character.destroy_all
+      Search.destroy_all
+      CharactersFacade.import_characters
+      @characters = Character.all
+    end
+
+  end
+
+  let(:valid_headers) do
+    { 'CONTENT_TYPE' => 'application/json; charset=utf-8' }
+  end
+
+  it "should return character records" do
+    before_size = Search.all.size
+    valid_params = {name: 'Darth Vader'}
+    post api_v1_characters_searches_path params: valid_params
+    expect(response).to be_successful
+    after_size = Search.all.size
+    body = JSON.parse(response.body, symbolize_names: true)
+    expect(before_size + 1).to eq(after_size)
+    expect(Search.last.name).to eq("Darth Vader")
+    expect(body[:data]).to be_an(Array)
+    expect(body[:data].first.keys).to eq([:id, :type, :attributes])
+    expect(body[:data].first[:id]).to be_a(String)
+    expect(body[:data].first[:type]).to be_a(String)
+    expect(body[:data].first[:attributes]).to be_a(Hash)
+    expect(body[:data].first[:attributes].keys).to eq([:name, :height, :mass, :hair_color, :skin_color, :eye_color, :birth_year, :gender])
+  end
+
+  it "should return 400 status and message if user inputs invalid search params" do
+    before_size = Search.all.size
+    valid_params = {name: 'Darth Vader', taller_than: "tall"}
+    post api_v1_characters_searches_path params: valid_params
+    expect(response).to_not be_successful
+    after_size = Search.all.size
+    expect(before_size).to eq(after_size)
+    expect(response.body).to eq("Search could not be executed")
+    expect(response.status).to eq(400)
+  end
+
+  it "should return 200 status and message if no records match the search" do
+    before_size = Search.all.size
+    valid_params = {name: 'Will Ferrell'}
+    post api_v1_characters_searches_path params: valid_params
+    expect(response).to be_successful
+    after_size = Search.all.size
+    expect(before_size + 1).to eq(after_size)
+    expect(response.body).to eq("No records matched the given search")
+  end
+
+end
