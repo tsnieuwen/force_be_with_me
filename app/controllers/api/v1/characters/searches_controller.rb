ea69bd1142159263fab57f44274b7fd517f41c40
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-06-23 11:05:35 -0600

    Testing for characte model

--- app/controllers/api/v1/characters/searches_controller.rb
+++ app/controllers/api/v1/characters/searches_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::Characters::SearchesController < ApplicationController
 
   def create
-    if Search.create(search_params)
+    search = Search.new(search_params)
+    if search.save
       searched_characters = Character.discover_characters(search_params)
       render json: CharactersSerializer.new(searched_characters)
     else
-      render json:
-    end 
+      render json: "Search could not be executed", status: 400
+    end
 
   end
 
